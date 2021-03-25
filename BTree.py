4a45d517be1a5451dd21a73f771d76e004a8a5fa
ahhon <polygon2000x@gmail.com>
2021-03-25 17:04:17 +0700

    finished BTree (untested)

--- BTree.py
+++ BTree.py
@@ -3,6 +3,29 @@ class BTree:
     def __init__(self, sz):
         self.size = sz
         self.arr = [0] * (sz + 1)
+        self.sums = [0] * (sz + 1)
+
+    def add(self, i, dx):
+        if i <= 0 or i > self.size: raise Exception(f'index out of range: {i}')
+        arr, sums = self.arr, self.sums
+        arr[i] += dx
+
+        while i <= self.size:
+            sums[i] += dx
+            i += i&-i
 
     def update(self, i, x):
-        pass
+        self.add(i, x - self.arr[i])
+
+    def sumAll(self, i=None):
+        if i is None: i = self.size
+        s = 0
+        while i > 0:
+            s += sums[i]
+            i -= i&-i
+
+        return s
+
+    def sumRange(self, i0, i1):
+        if i0 > i1: raise Exception(f'i0 > i1')
+        return self.sumAll(i1) - self.sumAll(i0-1)
